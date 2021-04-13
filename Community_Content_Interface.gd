64b8f68cba2e2310bd53f9ea205907dfb3b23e28
Noswad-Zip <64293615+Noswad-Zip@users.noreply.github.com>
2021-04-13 14:10:31 -0500

    Community Content Mostly Finished

--- Community_Content_Interface.gd
+++ Community_Content_Interface.gd
@@ -1,29 +1,40 @@
 extends CanvasLayer
 
 # Declare member variables here. Examples:
-const cc_url = "http://scresdir.appspot.com/resource" # The community content URL
+const cc_url = "https://scresdir.appspot.com/resource" # The community content URL
 var items = {}
 var NextCursor = null
+var requestwait = false
+
+func request_cc_data(Action, NextCursor):
+	if requestwait == true:
+		print("request wait")
+	else:
+		requestwait = true
+		
+		var http_request = HTTPRequest.new()
+		add_child(http_request)
+		http_request.connect("request_completed", self, "_http_request_completed")
+		
+		# Perform a POST request.
+		# Note: Don't make simultaneous requests using a single HTTPRequest node.
+		
+		var body = "Action=" + str(Action)
+		
+		if NextCursor != null:
+			body = body + "&" + "Cursor=" + str(NextCursor)
+		
+		#var body = "Action=list&UserId=Kaalus"
+		print(body)
+		var headers = ["Content-Type: application/x-www-form-urlencoded"]
+		
+		var error = http_request.request(cc_url, headers, false, HTTPClient.METHOD_POST, body)
+		if error != OK:
+			push_error("An error occurred in the HTTP request.")
 
 func _ready():
-	# Create an HTTP request node and connect its completion signal.
-	var http_request = HTTPRequest.new()
-	add_child(http_request)
-	http_request.connect("request_completed", self, "_http_request_completed")
-	
-	
-	# Perform a POST request.
-	# Note: Don't make simultaneous requests using a single HTTPRequest node.
-	
-	#var body = {"Action": "list"}
-	var body = "Action=list"
-	var headers = ["Content-Type: application/x-www-form-urlencoded"]
-	
-	var error = http_request.request(cc_url, headers, false, HTTPClient.METHOD_POST, body)
-	if error != OK:
-		push_error("An error occurred in the HTTP request.")
+	request_cc_data("list", NextCursor)
 
-# Called when the HTTP request is completed.
 func _http_request_completed(result, response_code, headers, body):
 	#var response = parse_json(body.get_string_from_utf8())
 	var response = body.get_string_from_utf8()
@@ -44,27 +55,49 @@ func _http_request_completed(result, response_code, headers, body):
 				print("Got NextCursor")
 				#print(thexml.get_attribute_value(0))
 				NextCursor = thexml.get_attribute_value(0)
-		
-		# Parses and stores the line as a dictionary entry
-		the_cc_response_dictionary[index] = {
-		Name = thexml.get_attribute_value(0),
-		Url = thexml.get_attribute_value(1),
-		Type = thexml.get_attribute_value(2),
-		Size = thexml.get_attribute_value(3),
-		RatingsAverage = thexml.get_attribute_value(4),
-		UserId = thexml.get_attribute_value(5),
-		ExtraText = thexml.get_attribute_value(6),
-		}
-		
-		index += 1
+		else:
+			
+			# Parses and stores the line as a dictionary entry
+			the_cc_response_dictionary[index] = {
+			Name = thexml.get_attribute_value(0),
+			Url = thexml.get_attribute_value(1),
+			Type = thexml.get_attribute_value(2),
+			Size = thexml.get_attribute_value(3),
+			RatingsAverage = thexml.get_attribute_value(4),
+			UserId = thexml.get_attribute_value(5),
+			ExtraText = thexml.get_attribute_value(6),
+			}
+			
+			index += 1
 		
 	add_cc_items_to_list(the_cc_response_dictionary, the_cc_response_dictionary.size())
+	requestwait = false
 
 func add_cc_items_to_list(cclist_dict_thingie, count_of_items):
 	for i in count_of_items:
-		var button1 = Button.new()
-		button1.text = str(cclist_dict_thingie[i]["Name"])
-		button1.show()
-		items[i] = button1
-		var buttoninstance = preload("res://CC_Item_Button.tscn").instance().init(cclist_dict_thingie[i])
-		#$ScrollContainer/VBoxContainer.add_child(items[i])
+#		var button1 = Button.new()
+#		button1.text = str(cclist_dict_thingie[i]["Name"])
+#		button1.show()
+#		items[i] = button1
+		
+		var buttoninstance = preload("res://CC_Item_Button.tscn").instance()
+		
+		buttoninstance.Name = cclist_dict_thingie[i]["Name"]
+		buttoninstance.Url = cclist_dict_thingie[i]["Url"]
+		buttoninstance.Type = cclist_dict_thingie[i]["Type"]
+		buttoninstance.Size = cclist_dict_thingie[i]["Size"]
+		buttoninstance.RatingsAverage = cclist_dict_thingie[i]["RatingsAverage"]
+		buttoninstance.ExtraText = cclist_dict_thingie[i]["ExtraText"]
+		
+		buttoninstance.init()
+		$ScrollContainer/VBoxContainer.add_child(buttoninstance)
+
+func _process(_delta):
+#	print("Vsbr pos = " + str($ScrollContainer.get_v_scrollbar().value))
+#	print("Vsbr max = " + str($ScrollContainer.get_v_scrollbar().max_value))
+#	print("Page = " + str($ScrollContainer.get_v_scrollbar().page))
+	
+	if $ScrollContainer.get_v_scrollbar().ratio >= 0.4:
+		print("past scroll")
+		request_cc_data("list", NextCursor)
+		
